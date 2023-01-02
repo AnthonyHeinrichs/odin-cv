3e13e6e856aee361184c4a07d97d7c316943eeee
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-01-02 17:52:07 +0100

    Added base styles and layout for form and cv preview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from './components/Header'
+import Form from './components/Form'
+import Cv from './components/Cv'
 import './App.css'
 
 function App() {
   return (
     <div>
       <Header />
+      <div className='main'>
+        <Form />
+        <Cv />
+      </div>
     </div>
   );
 }
---
+++ src/components/Cv.js
@@ -0,0 +1,15 @@
+import styles from './styles/Cv.module.css'
+
+const Cv = () => {
+  return (
+    <div className={styles.formMain}>
+      <div className={styles.title}>
+        <h1>Preview</h1>
+      </div>
+      <div className={styles.preview}>
+      </div>
+    </div>
+  )
+}
+
+export default Cv
