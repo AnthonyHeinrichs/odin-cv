531d7074a2ce41ab6bf2110890075c00ff6f5d87
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-01-02 17:24:31 +0100

    Updated header styling and text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styles from './styles/Header.module.css'
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1 className={styles.title}>CV Creator</h1>
+      <h1 className={styles.title}>CV Builder</h1>
     </div>
   )
 }
